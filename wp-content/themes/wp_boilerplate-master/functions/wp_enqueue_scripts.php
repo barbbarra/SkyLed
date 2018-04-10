8519a81d215a821f8b43dd3e89c3570aa72b1ace
Cristobal Dominguez <cristobald@gmail.com>
2018-04-10 17:32:43 -0300

    Elimina código no utilizado y agrega mejoras en Mapas de Google

--- wp-content/themes/wp_boilerplate-master/functions/wp_enqueue_scripts.php
+++ wp-content/themes/wp_boilerplate-master/functions/wp_enqueue_scripts.php
@@ -3,16 +3,6 @@
 	function dl_enqueue_scripts() {
 		$theme_data = wp_get_theme();
 
-		if (!class_exists( 'WooCommerce' )) {
-			/* Deregister Scripts */
-			wp_deregister_script( 'jquery' );
-			wp_deregister_script( 'jquery-migrate' );
-
-			/* Register Scripts */
-			wp_register_script('jquery', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.3.1', true);
-			wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jquery'), '3.0.0', true);
-		}
-
 		/* Register Scripts */
 		wp_register_script('flickity', get_theme_file_uri('/assets/js/lib/flickity.pkgd.js'), array('jquery-migrate'), '2.1.0', true);
 		wp_register_script('bootstrap', get_theme_file_uri('/assets/js/lib/bootstrap.js'), array('jquery-migrate'), null, true);
@@ -21,6 +11,8 @@
 		/* Enqueue Scripts */
 		wp_enqueue_script('flickity');
 		wp_enqueue_script('mainJS');
+		wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBuU_0_uLMnFM-2oWod_fzC0atPZj7dHlU&sensor=false');
+  	wp_enqueue_script('google-jsapi','https://www.google.com/jsapi');
 	}
 
 	add_action( 'wp_enqueue_scripts', 'dl_enqueue_scripts' );
