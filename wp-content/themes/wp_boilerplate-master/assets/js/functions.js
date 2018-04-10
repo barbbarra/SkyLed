6a78141bda329c5d7b4ba5d6c4bc313b10660672
Cristobal Dominguez <cristobald@gmail.com>
2018-04-10 18:26:43 -0300

    Arregla Problemas con Google Maps que evitaban que JS se siga ejecutando

--- wp-content/themes/wp_boilerplate-master/assets/js/functions.js
+++ wp-content/themes/wp_boilerplate-master/assets/js/functions.js
@@ -62,25 +62,4 @@ jQuery(document).ready( function($) {
     }
   });
 
-//mapa//
-function initMap() {
-              var uluru = {
-                  lat: -33.430860,
-                  lng: -70.616648
-              };
-              var map = new google.maps.Map(document.getElementById('contact-map'), {
-                  zoom: 14,
-                  center: uluru,
-                  scrollwheel: false
-              });
-              var marker = new google.maps.Marker({
-                  position: uluru,
-                  map: map,
-                  icon: 'https://easetemplate.com/free-website-templates/life-coach/images/map_marker.png'
-
-              });
-          }
-
- initMap()
-
 });
--- wp-content/themes/wp_boilerplate-master/functions/wp_enqueue_scripts.php
+++ wp-content/themes/wp_boilerplate-master/functions/wp_enqueue_scripts.php
@@ -8,11 +8,21 @@
 		wp_register_script('bootstrap', get_theme_file_uri('/assets/js/lib/bootstrap.js'), array('jquery-migrate'), null, true);
 		wp_register_script('mainJS', get_theme_file_uri('/assets/js/functions.js'), array('jquery-migrate', 'bootstrap'), $theme_data->get( 'Version' ), true);
 
+		wp_register_script('google', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBuU_0_uLMnFM-2oWod_fzC0atPZj7dHlU&sensor=false', null, null, true);
+		wp_register_script('googleApi', 'https://www.google.com/jsapi', null, null, true);
+		wp_register_script('maps', get_theme_file_uri('/assets/js/google_map.js'), null, null, true);
+
 		/* Enqueue Scripts */
 		wp_enqueue_script('flickity');
 		wp_enqueue_script('mainJS');
-		wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBuU_0_uLMnFM-2oWod_fzC0atPZj7dHlU&sensor=false');
-  	wp_enqueue_script('google-jsapi','https://www.google.com/jsapi');
+
+		if (is_home() || is_front_page()) {
+			wp_enqueue_script('google');
+			wp_enqueue_script('googleApi');
+			wp_enqueue_script('maps');
+		}
+
+
 	}
 
 	add_action( 'wp_enqueue_scripts', 'dl_enqueue_scripts' );
